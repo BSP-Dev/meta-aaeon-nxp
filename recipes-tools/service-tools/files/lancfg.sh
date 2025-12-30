0672c01a7da3ca4763efc1887f0878cb713a2d0f
aaeon-ecbu <jasonfsyang.aaeon.com.tw>
2025-12-30 13:32:23 +0800

    @MODEL:[SRG-IMX8PL, PICO-IMX8PL] @NEW: modify lancfg.sh & setup_gpios.sh @EFFECT: ethernet, USB VBus

--- recipes-tools/service-tools/files/lancfg.sh
+++ recipes-tools/service-tools/files/lancfg.sh
@@ -5,41 +5,41 @@ function  write_LAN_mac_address()
     error_mac1="00:00:00:00:00:00"
     error_mac2="FF:FF:FF:FF:FF:FF"
 
-    O_ETH0=$(sudo ifconfig -a eth0 | awk '/ether/ { print $2 }')
-    O_ETH1=$(sudo ifconfig -a eth1 | awk '/ether/ { print $2 }')
+    O_ETH0=$(ifconfig -a eth0 | awk '/ether/ { print $2 }')
+    O_ETH1=$(ifconfig -a eth1 | awk '/ether/ { print $2 }')
 
     ETH0_TMP=$(/home/srt/eeprom/srgimx8cfg -0 )
     ETH1_TMP=$(/home/srt/eeprom/srgimx8cfg -1 )
 
     calcheckSum=$(/home/srt/eeprom/srgimx8cfg -p )
-	readcheckSum=$(/home/srt/eeprom/srgimx8cfg -k )
+    readcheckSum=$(/home/srt/eeprom/srgimx8cfg -k )
 
 
     if ( [[ "$ETH0_TMP" == "$error_mac1" ]] || [[ "$ETH0_TMP" == "$error_mac2" ]] ) ; then
-			sleep 1
-            return
+        sleep 1
+        return
     elif ( [[ "$ETH1_TMP" == "$error_mac1" ]] || [[ "$ETH1_TMP" == "$error_mac2" ]] ) ; then
-			sleep 1
-            return
-	elif [ "$calcheckSum" != "$readcheckSum" ]; then
-			sleep 1
-            return
+        sleep 1
+        return
+    elif [ "$calcheckSum" != "$readcheckSum" ]; then
+        sleep 1
+        return
     else
-			sleep 3
-            N_ETH0=$ETH0_TMP
-            N_ETH1=$ETH1_TMP
-			sudo ifconfig eth0 down
-			sudo ifconfig eth1 down
+        sleep 3
+        N_ETH0=$ETH0_TMP
+        N_ETH1=$ETH1_TMP
+        ifconfig eth0 down
+        ifconfig eth1 down
 
-			address=$O_ETH0
-			sudo ip link set dev eth0 address ${N_ETH0}
+        address=$O_ETH0
+        ip link set dev eth0 address ${N_ETH0}
 
-			address=$O_ETH1
-			sudo ip link set dev eth1 address ${N_ETH1}
+        address=$O_ETH1
+        ip link set dev eth1 address ${N_ETH1}
 
-			sleep 1.2
-			sudo ifconfig eth0 up
-			sudo ifconfig eth1 up
+        sleep 1.2
+        ifconfig eth0 up
+        ifconfig eth1 up
     fi
 
 }
@@ -48,19 +48,18 @@ function  write_LAN_mac_address()
 function  write_LAN_LED_setting()
 {
 
-    sudo /usr/sbin/mdio-tool w eth0 0x1f 0x0d04
-    sudo /usr/sbin/mdio-tool w eth0 0x10 0xC048
-    sudo /usr/sbin/mdio-tool w eth0 0x1f 0x0000
-    sudo /usr/sbin/mdio-tool w eth1 0x1f 0x0d04
-    sudo /usr/sbin/mdio-tool w eth1 0x10 0xC048
-    sudo /usr/sbin/mdio-tool w eth1 0x1f 0x0a42
+    /usr/sbin/mdio-tool w eth0 0x1f 0x0d04
+    /usr/sbin/mdio-tool w eth0 0x10 0xC048
+    /usr/sbin/mdio-tool w eth0 0x1f 0x0000
+    /usr/sbin/mdio-tool w eth1 0x1f 0x0d04
+    /usr/sbin/mdio-tool w eth1 0x10 0xC048
+    /usr/sbin/mdio-tool w eth1 0x1f 0x0a42
 }
 
 function main()
 {
     write_LAN_mac_address
     write_LAN_LED_setting
-    i2ctransfer -f -y 2 w2@0x51 0x02 0x12
 }
 
 main
